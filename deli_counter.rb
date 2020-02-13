83f61a912382ffc8694f87fd8dfdf4df2f7de280
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-13 05:15:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(array) 
+  if array.length >= 1
+    yee = []
+    counter = 1 
+    array.each do |name|
+      yee<<"#{counter}. #{name}"
+      counter += 1 
+    end 
+    puts "The line is currently: #{yee.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array<< name 
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+   if array.length == 0 
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}." 
+    array.shift 
+  end
+end
